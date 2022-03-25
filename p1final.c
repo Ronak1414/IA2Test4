d80af75ba7e8f2de8f3cb52057d7610fbc511084
Ronak <96277743+Ronak1414@users.noreply.github.com>
2022-03-25 03:22:41 +0000

    p1 done !!

--- p1final.c
+++ p1final.c
@@ -1,1 +1,49 @@
 #include <stdio.h>
+#include<math.h>
+float input_radius()
+{
+  float r;
+  printf("enter the radius:\n");
+  scanf("%f",&r);
+  return r;
+}
+
+float input_heigth()
+{
+  float h;
+  printf("enter the height:\n");
+  scanf("%f",&h);
+  return h;
+}
+
+float input_length()
+{
+  float l;
+  printf("enter the lenght:\n");
+  scanf("%f",&l);
+  return l;
+}
+
+float find_weight(float radius, float height, float length)
+{
+  float w;
+  w=3.14*radius*radius*radius*sqrtf(height * length);
+  return w;
+}
+
+void output(float radius, float height, float length, float weight)
+{
+  printf("The weight of the camel with radius : %f , height : %f , length : %f is \n%f",radius,height,length,weight);
+}
+
+int main()
+{
+   float radius,height,length,weight;
+
+  radius=input_radius();
+  height=input_heigth();
+  length = input_length();
+ weight=find_weight( radius,height, length);
+ output(radius,height,length,weight);
+  return 0;
+}
--- p2final.c
+++ p2final.c
@@ -1,1 +1,18 @@
 #include<stdio.h>
+#include<math.
+typedef struct camel {
+float r, h, l, w;
+} C;
+
+c input()
+{
+  printf("enter the radius:\n");
+  scanf("%f",&c.r);
+  printf("enter the height:\n");
+  scanf("%f",&c.h);
+  printf("enter the lenght:\n");
+  scanf("%f",&c.l);  
+}
+
+c 
+
